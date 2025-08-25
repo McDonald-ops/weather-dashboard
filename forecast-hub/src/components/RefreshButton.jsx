bc8536a324eec22878ac196ea5d93183cc59d3f0
McDonald <odiasemcdonald1@gmail.com>
2025-08-25 15:31:08 +0100

    Added a manual refresh button

--- forecast-hub/src/components/RefreshButton.jsx
+++ forecast-hub/src/components/RefreshButton.jsx
@@ -1,1 +1,13 @@
 // Manual refresh button
+function RefreshButton({ onRefresh }) {
+  return (
+    <button
+      onClick={onRefresh}
+      className="bg-blue-600 px-4 py-2 rounded-lg text-white font-semibold hover:bg-blue-700 transition"
+    >
+      🔄 Refresh
+    </button>
+  );
+}
+
+export default RefreshButton;
